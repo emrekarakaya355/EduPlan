a079988f4ed61cbf6508c88e8310d493bbe40ecf
Emre Karakaya <emrekarakaya35.5@gmail.com>
2025-04-28 22:06:36 +0300

    gridstack eklendi.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,6 @@
 import './bootstrap';
 import Swal from 'sweetalert2';
 window.Swal = Swal;
+import 'gridstack/dist/gridstack.min.css';
+import { GridStack } from 'gridstack';
+window.GridStack = GridStack;
